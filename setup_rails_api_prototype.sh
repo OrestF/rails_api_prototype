25f71ef85a6ea7bf17f4b6ea5363b9d9992f249d
Orest Falchuk <orest@Orests-iMac.local>
2019-02-21 15:34:38 +0200

    Add url

--- setup_rails_api_prototype.sh
+++ setup_rails_api_prototype.sh
@@ -5,7 +5,7 @@ echo 'Loading...'
 gem install bundler
 gem install rails -v $1
 
-# git clone https://github.com/OrestF/rails_api_prototype.git
-rails new $2 --api -d postgresql -T --skip-coffee -m template.rb
-# rm -rf ./rails_prototype
+git clone https://github.com/OrestF/rails_api_prototype.git
+rails new $2 --api -d postgresql -T --skip-coffee -m https://raw.githubusercontent.com/OrestF/rails_api_prototype/master/template.rb
+rm -rf ./rails_prototype
 echo 'Done!'
